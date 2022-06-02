b62f4e1936a6245e9b51e634ea17a3b2a551fb65
Fawzy20 <fawzy-elsam@outlook.fr>
2022-06-02 11:46:38 +0200

    ajout des button

--- src/component/TirageCarte/TirageCarte.js
+++ src/component/TirageCarte/TirageCarte.js
@@ -2,6 +2,7 @@ import logo from "../../asset/logo.png"
 import './TirageCarte.css';
 import { useState, useEffect } from "react";
 import api from '../../api/api';
+import { Button } from "react-bootstrap";
 
 function Tirage() {
 
@@ -9,37 +10,46 @@ function Tirage() {
     const [random, setRandom] = useState([])
 
     const fetchrandom = () => {
-        const value = Object.keys(cartes);
-        const randIndex = Math.floor(Math.random() * value.length)
-        const randKey = value[randIndex]
-        setRandom(cartes[randKey])
+        const keys = Object.keys(cartes);
+        const randIndex = Math.floor(Math.random() * keys.length)
+        const randKey = keys[randIndex]
+        setRandom( cartes[randKey])
+        console.log(cartes[randIndex]);
+
     }
-    
+
     const fetchCarte = async () => {
         const { data } = await api.get()
         setCarte(data.pokemons)
+        fetchrandom()
     }
 
     useEffect(() => {
         fetchCarte()
     }, [])
 
-    useEffect(() => {
-        fetchrandom()
-    }, [])
-
-    console.log(random);
+    console.log();
 
     return (
         <div className='container-fluid' >
             <div className='container' >
                 <div className='row tirage' >
                     <div className='logo' >
-                        <img src={logo} alt='' />
+                        <img src={logo} heigh="139" width="375" alt='' />
                     </div>
-                    {
-                   
-                    }
+                    <div className="carteTirage">
+                        {
+
+                        }
+                    </div>
+                    <div className="button-block" >
+                        <div>
+                            <Button className="btn-lancer" variant="warning">Lancer</Button>
+                        </div>
+                        <div>
+                            <Button className="btn-stop" variant="warning">Stop</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
